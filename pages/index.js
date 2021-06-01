6667e2a6fd25d6efa22a331882cc9d683e87ecbb
Lucas Duarte <lucasduarteinacio@hotmail.com>
2021-05-31 22:17:05 -0300

    alteraçoes

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
